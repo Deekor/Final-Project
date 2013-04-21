e6648b5637b5ba9824a93c421bb653aa94af83e1
Dee Evans <deekor88@gmail.com>
2013-04-21 17:29:54 -0600

    leaving sunday

--- test.cc
+++ test.cc
@@ -3,6 +3,7 @@
 #include <boost/property_tree/ptree.hpp>
 #include <boost/property_tree/xml_parser.hpp>
 #include <sstream>
+#include <boost/foreach.hpp>
 
 using namespace std;
 
@@ -10,21 +11,30 @@ using namespace std;
 int main(int argc, char const *argv[])
 {
     boost::property_tree::ptree pt;
+    boost::property_tree::ptree pt1;
+    boost::property_tree::ptree pt2;
+    boost::property_tree::ptree pt3;
 
-    pt.put("spreadsheet.cell.name", "a2");
-    pt.put("spreadsheet.cell.contents", "adsf");
+    pt1.put("spreadsheet.cell.name", "a2");
+    pt1.put("spreadsheet.cell.contents", "adsf");
+
+    pt2.put("spreadsheet.cell.name", "d6");
+    pt2.put("spreadsheet.cell.contents", "345");
+
+    pt3.put("spreadsheet.cell.name", "d2");
+    pt3.put("spreadsheet.cell.contents", "=d6");
+
+    pt.add_child("spreadsheet.cell", pt1);
+    pt.add_child("spreadsheet.cell", pt2);
+    pt.add_child("spreadsheet.cell", pt3);
 
     write_xml("output.xml", pt);
 
-    boost::property_tree::ptree ptr;
-    read_xml("output.xml", ptr);
-
-    ptr.put("spreadsheet.cell.name", "d6");
-    ptr.put("spreadsheet.cell.contents", "345");
-    ptr.put("spreadsheet.cell.name", "d2");
-    ptr.put("spreadsheet.cell.contents", "=d6");
-
-    write_xml("output2.xml", ptr);
+    BOOST_FOREACH(const boost::property_tree::ptree::value_type &v, pt.get_child("spreadsheet")) 
+    {
+       string name = v.second.first.get_value("name");
+       cout << name << endl;
+    }
 	
 	return 0;
 }
